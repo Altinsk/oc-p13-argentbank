e57f28dc8f18576f42c0fa12e66fd3f6e0af45f4
Omar Rabie <omar.rabie@hotmail.fr>
2024-01-15 14:32:26 +0100

    added pages

---
+++ bank/src/pages/SignIn/index.jsx
@@ -0,0 +1,10 @@
+/* COMPONENTS */
+import Form from '../../components/SignInForm/index';
+
+export default function SignIn() {
+    return (
+        <main className="bgDark">
+            <Form />
+        </main>
+    );
+}
---
+++ bank/src/pages/User/index.jsx
@@ -0,0 +1,46 @@
+/* IMPORTS */
+import { selectUserIsConnected } from '../../store/selectors/user';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
+/* COMPONENTS */
+import UserHeader from '../../components/UserHeader/index';
+import AccountsList from '../../components/AccountsList/index';
+
+/* DATA */
+const ACCOUNTS = [
+    {
+        title: 'Argent Bank Checking (x8349)',
+        amount: 2082.79,
+        description: 'Available Balance',
+    },
+    {
+        title: 'Argent Bank Savings (x6712)',
+        amount: 10928.42,
+        description: 'Available Balance',
+    },
+    {
+        title: 'Argent Bank Credit Card (x8349)',
+        amount: 184.3,
+        description: 'Current Balance',
+    },
+];
+
+export default function User() {
+    const navigate = useNavigate();
+    const isConnected = useSelector(selectUserIsConnected());
+
+    useEffect(() => {
+        if (!isConnected) {
+            navigate('/');
+        }
+    }, [isConnected]);
+
+    return (
+        <main className="bgDark">
+            <UserHeader />
+            <AccountsList accounts={ACCOUNTS} />
+        </main>
+    );
+}
